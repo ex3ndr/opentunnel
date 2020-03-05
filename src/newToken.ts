968c7196520244064ae5d741d2f0c48c0f4d5bce
Steve Korshakov <steve@korshakov.com>
2020-03-05 00:56:31 -0800

    feat: Switch domain

--- src/newToken.ts
+++ src/newToken.ts
@@ -1,7 +1,14 @@
 import nacl from 'tweetnacl';
 import { BufferWriter } from './utils/BufferWriter';
 
+const reservedNames = ['backhaul', 'registrator', 'api'];
+
 export function newToken(privateKey: string, host: string, expire: number = 0) {
+    for (let r of reservedNames) {
+        if (host.toLowerCase().startsWith(r.toLowerCase() + '.')) {
+            throw Error('Reserved subdomain');
+        }
+    }
     let writer = new BufferWriter();
     writer.appendUInt16(host.length);
     writer.appendAsciiString(host);
