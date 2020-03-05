b79d7041a1847f2889133244228fb55c0a09fcdb
Steve Korshakov <steve@korshakov.com>
2020-03-05 11:55:22 -0800

    feat: Export ManagedTunnel for programmatic use

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { newKey } from './newKey';
 export { newToken } from './newToken';
 export { registerRandomDomain } from './registrator/registerRandomDomain';
+export { ManagedTunnel } from './client/ManagedTunnel';
